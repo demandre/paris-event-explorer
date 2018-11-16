462e5617900ecc35724cc440d87217bdb30aa3a1
Timmy K <tkhamsithideth@gmail.com>
2018-11-16 12:46:37 +0100

    Add event object, Transform json to Event objects list and correct some bugs peer with david and me

--- search-engine/search-engine.js
+++ search-engine/search-engine.js
@@ -22,7 +22,7 @@ var Event = function Event(id, title="", description="", place="", startDate="",
  *  Class SearchEngine: A class to manage API request with a search bar.  
  */
 var SearchEngine = function SearchEngine () {
-  
+  this.eventRepository = [];
 }
 
 /**
@@ -33,16 +33,16 @@ SearchEngine.prototype.init = function () {
   var elSearchBar = document.querySelector('#searchBar');
 
   elClick.addEventListener('click', function() {
-    checkSearch(elSearchBar.value);
-  })
+    this.checkSearch(elSearchBar.value);
+  }.bind(this))
 
   elSearchBar.addEventListener('keypress', function(e) {
     var key = e.which || e.keyCode;
 
     if (key === 13) {
-      checkSearch(elSearchBar.value);
+      this.checkSearch(elSearchBar.value);
     } 
-  })
+  }.bind(this))
 }
 
 /**
@@ -53,11 +53,11 @@ SearchEngine.prototype.checkSearch = function (search) {
   var regexSearch = /[a-zA-Z-0-9-\/]+/;
 
   if (regexSearch.test(search)) {
-    this.jsonToEvents(this.doODPAPIRequest(search));
+    this.doODPAPIRequest(search);
   }
 
   if (!regexSearch.test(search)) {
-    document.getElementById('search-info').innerHTML = "Le format de recherche est incorrect! Seul les nombres, les lettres, '/' et '-' sont autorisés";
+    console.log("Le format de recherche est incorrect! Seul les nombres, les lettres, '/' et '-' sont autorisés");
   }
 }
 
@@ -71,10 +71,8 @@ SearchEngine.prototype.doODPAPIRequest = function (requestParam) {
   var request = "https://opendata.paris.fr/api/records/1.0/search/?dataset=evenements-a-paris&q="+requestParam+"&rows=20&refine.tags=Paris";
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      jsonToEvents(this.responseText);
-      return this.responseText;
+          searchEngine.jsonToEvents(this.responseText);
     }
-    return false;
   };
   xhttp.open("GET", request, true);
   xhttp.send();
@@ -84,9 +82,29 @@ SearchEngine.prototype.doODPAPIRequest = function (requestParam) {
  * Allows you to tranform json to Event objects
  * @params {String} json
  */
-function jsonToEvents(json) {
-  console.log(json);
+SearchEngine.prototype.jsonToEvents = function(json) {
+  var ourData = JSON.parse(json);
+  this.eventRepository = [];
+  ourData.records.forEach(function(el,id) {
+      this.eventRepository.push(
+        new Event(
+        id,
+        el.fields.title,
+        el.fields.description,
+        el.fields.placename,
+        el.fields.date_start,
+        el.fields.date_end,
+        el.fields.link,
+        el.fields.period,
+        el.fields.address,
+        el.fields.city,
+        el.fields.departement,
+        el.fields.price
+        )
+      );
+  }.bind(this));
 }
 
+var searchEngine = new SearchEngine();
 
-SearchEngine.prototype.init();
+searchEngine.init();
